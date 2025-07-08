7c7a8e088a040abc1227004f74d1dd994d5097fb
Muhammad Khubaib Ahmad <muhammadkhubaibahmad854@gmail.com>
2025-07-08 10:16:39 -0400

    Update gemini_api.py

--- scripts/gemini_api.py
+++ scripts/gemini_api.py
@@ -2,7 +2,7 @@ def generate_answer_from_chunks(chunks, question):
     import os
     import google.generativeai as genai
 
-    genai.configure(api_key = os.getenv"GEMINI_API_KEY")
+    genai.configure(api_key = os.getenv("GEMINI_API_KEY"))
     gemini_model = genai.GenerativeModel("gemini-2.0-flash")
     context = "\n\n".join(chunks)
     prompt = f"""Use the context below to answer the question.
