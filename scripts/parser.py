2a56d010aa6d04a0a07f46274dacc47e40285d6a
Muhammad Khubaib Ahmad <muhammadkhubaibahmad854@gmail.com>
2025-07-08 10:26:56 -0400

    Update parser.py

--- scripts/parser.py
+++ scripts/parser.py
@@ -4,24 +4,24 @@ from PyPDF2 import PdfReader
 import os
 
 
-def extract_text_from_file(uploaded_dict):
-    filename = next(iter(uploaded_dict))  # Get filename string
-    content = uploaded_dict[filename]     # Get binary content
-    ext = os.path.splitext(filename)[1].lower()
+def extract_text_from_file(uploaded_file):
+    # filename = next(iter(uploaded_dict))  # Get filename string
+    # content = uploaded_dict[filename]     # Get binary content
+    ext = os.path.splitext(uploaded_file.name)[1].lower()
 
     if ext == '.txt':
         return content.decode('utf-8')
 
     elif ext == '.docx':
-        doc = Document(BytesIO(content))
+        doc = Document(uploaded_file)
         return '\n'.join([p.text for p in doc.paragraphs])
 
     elif ext == '.pdf':
-        reader = PdfReader(BytesIO(content))
+        reader = PdfReader(uploaded_file)
         text = ''
         for page in reader.pages:
             text += page.extract_text() + '\n'
         return text
 
     else:
-        raise ValueError("Unsupported file type.")
+        raise ValueError("Unsupported file type. Please upload .txt, .docx, or .pdf")
