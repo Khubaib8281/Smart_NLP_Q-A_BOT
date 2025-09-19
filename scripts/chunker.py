c91d4f33a07f9a1e3fcd35c8636cdcbae8a639fe
Muhammad Khubaib Ahmad <muhammadkhubaibahmad854@gmail.com>
2025-09-19 00:06:32 -0400

    import solve.py

--- scripts/chunker.py
+++ scripts/chunker.py
@@ -1,9 +1,10 @@
-# def chunk_text(text):
-#     import re
-#     paragraphs = re.split(r'\n\s*\n', text.strip())
-#     chunks = [p.strip().replace('\n', ' ') for p in paragraphs if p.strip()]
-#     return chunks
-
+try:
+    # new package split
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+except ImportError:
+    # fallback for older versions
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+    
 def chunk_text(text: str, chunk_size=1000, overlap=100):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
