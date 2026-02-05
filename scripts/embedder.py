4af4a3bf8f199b52fbcdd56f93d2d6ebfedb2d72
Muhammad Khubaib Ahmad <muhammadkhubaibahmad854@gmail.com>
2026-02-05 08:39:41 +0500

    embs

--- scripts/embedder.py
+++ scripts/embedder.py
@@ -3,5 +3,5 @@ from sentence_transformers import SentenceTransformer
 embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def embed_text(chunks):
-  return embedding_model.encode(chunks)
+  embeddings = embedding_model.encode(chunks)
   return np.array(embeddings).astype('float32')
